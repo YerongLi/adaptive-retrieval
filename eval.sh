42060665906a5a4314a19ed8ac0fb9db240113d8
Yerong Li <yerong.li@outlook.com>
2024-04-05 13:48:09 -0500

    eval.sh

--- eval.sh
+++ eval.sh
@@ -1,5 +1,6 @@
-CUDA_VISIBLE_DEVICES=2 python run_model.py \
+CUDA_VISIBLE_DEVICES=2,4,6 python run_model.py \
     --model_name /data/gpt-neox-20b \
     --input_file data/popQA.tsv \
     --eval_method vanilla \
-    --int8bit
+    --int8bit \
+    --parallel 3
