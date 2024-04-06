ce47ed72da97e0cc6e5d56749442e41ac7dbdba8
Yerong Li <yerong.li@outlook.com>
2024-04-06 02:29:12 -0500

    Upload

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=2,4,6 python run_model.py \
-    --model_name /data/gpt-neox-20b \
+    --model_name /data/opt-13b \
     --input_file data/popQA.tsv \
-    --eval_method vanilla \
-    --int8bit \
+    --eval_method contriever \
+    --ret_file contriever_results.jsonl \
     --sample 800
