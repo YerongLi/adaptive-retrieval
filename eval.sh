6cef5b76f2ffa104c9fd025658544fb293bd249b
Yerong Li <yerong.li@outlook.com>
2024-04-06 02:30:23 -0500

    Use ret_path instead of ret_file

--- eval.sh
+++ eval.sh
@@ -2,5 +2,5 @@ CUDA_VISIBLE_DEVICES=2,4,6 python run_model.py \
     --model_name /data/opt-13b \
     --input_file data/popQA.tsv \
     --eval_method contriever \
-    --ret_file contriever_results.jsonl \
+    --ret_path contriever_results.jsonl \
     --sample 800
